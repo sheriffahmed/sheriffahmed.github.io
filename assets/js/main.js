d799266ddf7316d602260d5e512d8ad051294a13
sheriffahmed <sheriff.v.ahmed@gmail.com>
2022-08-26 11:48:42 -0400

    Type animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@
   }
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
@@ -159,6 +159,23 @@
   });
 
   /**
+   * Hero type effect
+   */
+   const typed = select('.typed')
+   if (typed) {
+     let typed_strings = typed.getAttribute('data-typed-items')
+     typed_strings = typed_strings.split(',')
+     new Typed('.typed', {
+       strings: typed_strings,
+       loop: true,
+       typeSpeed: 100,
+       backSpeed: 50,
+       backDelay: 2000
+     });
+   }
+
+
+  /**
    * Skills animation
    */
   let skilsContent = select('.skills-content');
@@ -222,7 +239,7 @@
   });
 
   /**
-   * Initiate portfolio lightbox 
+   * Initiate portfolio lightbox
    */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
@@ -246,7 +263,7 @@
   });
 
   /**
-   * Initiate Pure Counter 
+   * Initiate Pure Counter
    */
   new PureCounter();
 
